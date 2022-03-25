0f1606116bc83e66f6be7a9ad53bbaae88769fcd
Ilija <79417610+Shinigaaami@users.noreply.github.com>
2022-03-25 15:29:18 +0100

    Fixed bug on close

--- dist/script/script.js
+++ dist/script/script.js
@@ -7,7 +7,6 @@ for (let concert of concerts) {
     p.style.backgroundColor = " rgba(0, 0, 0, 0.92)";
     p.style.borderColor = "#991825 ";
   });
-
   concert.addEventListener("mouseleave", () => {
     let p = concert.querySelector("a>p");
     p.style.color = "rgba(182, 182, 182, 0.296) ";
@@ -15,19 +14,17 @@ for (let concert of concerts) {
     p.style.backgroundColor = "rgba(0, 0, 0, 0) ";
   });
 }
-
 const songs = document.getElementsByClassName("songContainer");
 const songPlaying = document.getElementById("songToPlay");
 const buttonsExit = document.getElementsByClassName("xIcon");
 let exitBt;
 let songPlayingDiv;
-
-for (const song of songs) {
+for (let song of songs) {
   song.addEventListener("click", () => {
-    for (let i = 0; i < songs.length; i++) {
+    for (let i = 0; i < songs.length; i += 1) {
       const element = songs[i];
       let button = element.getElementsByTagName("i")[0];
-
+      button.nextElementSibling.classList.add("hide");
       element.classList.remove("playing");
       if (element == song) {
         let songName = song
@@ -35,30 +32,21 @@ for (const song of songs) {
           .textContent.toLowerCase();
         songName = songName.replace(/\s+/g, "");
         songPlayingDiv = song;
-
         song
           .querySelector(" div > p:nth-child(2)")
           .classList.add("playingText");
-        console.log(songPlaying);
         button.nextElementSibling.classList.remove("hide");
-
-        console.log(songPlaying.src);
-        // break;
         if (button.classList.contains("gg-play-button-o")) {
-          //*Have to have this evil...*/
           if (
             songPlaying.src == "audio/" + songName + ".mp3" ||
             songPlaying.src.includes("audio/" + songName + ".mp3")
           ) {
             playSong(button);
-            console.log("Play song ONE MORE TIME");
           } else {
-            console.log(songName);
             songPlaying.src = "audio/" + songName + ".mp3";
             songPlaying.currentTime = 0.1;
-            songPlaying.volume = 0.6;
+            songPlaying.volume = 0.1;
             playSong(button);
-            console.log("Play FIRST TIME");
           }
         } else if (button.classList.contains("gg-play-pause-o")) {
           pauseSong(button);
@@ -67,45 +55,35 @@ for (const song of songs) {
       } else {
         button.classList.add("gg-play-button-o");
         button.classList.remove("gg-play-pause-o");
-        // song.classList.remove("playing");
       }
     }
   });
 }
-
 for (const bt of buttonsExit) {
   bt.addEventListener("click", (e) => {
     songPlayingDiv.children[1].children[1].classList.remove("playingText");
     songPlaying.src = "";
     songPlayingDiv.classList.remove("playing");
-    /*Returning previously played song to default text color*/
     bt.classList.add("hide");
-    console.log("close window");
-
     let playButtons = document.querySelectorAll(".songContainer>div>i");
     playButtons.forEach(addPlaying);
     e.stopPropagation();
   });
 }
-
 function playSong(button) {
   button.classList.remove("gg-play-button-o");
   button.classList.add("gg-play-pause-o");
   button.nextElementSibling.classList.remove("hide");
   songPlaying.play();
 }
-
 function addPlaying(btn) {
   btn.className = "gg-play-button-o";
-  console.log(btn);
 }
-
 function pauseSong(button) {
   button.classList.add("gg-play-button-o");
   button.classList.remove("gg-play-pause-o");
   songPlaying.pause();
 }
-
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     songPlayingDiv.classList.remove("playing");
@@ -116,3 +94,4 @@ document.addEventListener("keydown", (e) => {
     e.stopPropagation();
   }
 });
+
